83a6e555264b804948dd6102b4b536dcfd0af115
YariMaco <96464552+YariMaco@users.noreply.github.com>
2024-04-08 15:22:59 -0600

    Crud proveedores completo

--- app/(dashboard)/proveedores/page.jsx
+++ app/(dashboard)/proveedores/page.jsx
@@ -1,15 +1,22 @@
 'use client';
 
-import Agregar from "@/app/components/inventario/crearProducto";
-import Eliminar from "../../components/inventario/eliminarProducto";
+import Agregar from "@/app/components/proveedor/crear";
+import Eliminar from "../../components/proveedor/eliminar";
+import Editar from "@/app/components/proveedor/editar";
+
+
 import Buscador from "../../components/pos/buscador";
-import { CirclePlus, FileUp, Pencil, SlidersHorizontal, Trash } from "lucide-react";
+import { CirclePlus, FileUp, Pencil, SlidersHorizontal, Trash,Eye } from "lucide-react";
 import { useState } from "react";
-import Filtro from "../../components/inventario/filtro";
+import Ver from "../../components/proveedor/ver";
 
 export default function Proveedores() {
   const [open, setOpen] = useState(false);
   const [agregar, setAgregar] = useState(false);
+  const [ver, setVer] = useState(false);
+  const [editar, setEditar] = useState(false);
+
+  
 
   return (
     <>
@@ -40,103 +47,85 @@ export default function Proveedores() {
                 <tr>
                   <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Id</th>
                   <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Nombre</th>
-                  <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Estado</th>
-                  <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Proveedor</th>
-                  <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Cantidad</th>
-                  <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Fecha ingreso</th>
-                  <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Categoría</th>
+                  <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Teléfono</th>
+                  <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Tipo</th>
+                  <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Correo Electrónico</th>
                   <th className="text-sm font-semibold text-gray-600 dark:text-gray-400 pb-4">Acciones</th>
                 </tr>
               </thead>
               <tbody>
                 <tr className="border-b dark:border-gray-600">
-                  <td className="text-sm text-gray-900 dark:text-gray-200 py-4">10001</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">Refresco Coca Cola 3L</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">
-                    <span className="px-2 py-1 text-xs font-medium uppercase tracking-wide text-yellow-800 bg-yellow-200 dark:text-yellow-200 dark:bg-yellow-800 rounded-full">Por caducar</span>
-                  </td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200 py-4">PR1001</td>
                   <td className="text-sm text-gray-900 dark:text-gray-200">Coca Cola</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">1000</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">16/10/2021</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">800-SIEMPRE (743-6773)</td>
                   <td className="text-sm text-gray-900 dark:text-gray-200">Bebidas</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">comunicacion@femsa.com.cr</td>
                   <td className="flex gap-2 justify-center my-2">
-                    <button className="p-2 text-gray-900 dark:text-gray-200 active:scale-95 transition-transform ease-in-out duration-75 hover:scale-105 transform bg-blue-500 dark:bg-blue-600 rounded-md">
-                      <Pencil size={18} strokeWidth={2} />
-                    </button>
-                    <button className="p-2 text-gray-900 dark:text-gray-200 active:scale-95 transition-transform ease-in-out duration-75 hover:scale-105 transform bg-red-500 dark:bg-red-600 rounded-md" onClick={() => setOpen(true)}>
-                      <Trash size={18} strokeWidth={2} />
-                    </button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-blue-600 bg-opacity-50 rounded-md " onClick={() => setEditar(true)}><Pencil size={15} strokeWidth={2.2} /></button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-green-600 bg-opacity-50 rounded-md" onClick={() => setVer(true)}><Eye size={15} strokeWidth={2.2} /> </button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-red-600 bg-opacity-50 rounded-md" onClick={() => setOpen(true)}><Trash size={15} strokeWidth={2.2} /> </button>
+                  </td>
+                </tr>
+                <tr className="border-b dark:border-gray-600">
+                  <td className="text-sm text-gray-900 dark:text-gray-200 py-4">PR1002</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">PepsiCo</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">800-PEPSICO</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">Bebidas</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">contacto@pepsico.com</td>
+                  <td className="flex gap-2 justify-center my-2">
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-blue-600 bg-opacity-50 rounded-md " onClick={() => setEditar(true)}><Pencil size={15} strokeWidth={2.2} /></button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-green-600 bg-opacity-50 rounded-md" onClick={() => setVer(true)}><Eye size={15} strokeWidth={2.2} /> </button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-red-600 bg-opacity-50 rounded-md" onClick={() => setOpen(true)}><Trash size={15} strokeWidth={2.2} /> </button>
                   </td>
                 </tr>
 
+                <tr className="border-b dark:border-gray-600">
+                  <td className="text-sm text-gray-900 dark:text-gray-200 py-4">PR1003</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">Nestlé</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">800-NESTLE</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">Alimentos</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">contacto@nestle.com</td>
+                  <td className="flex gap-2 justify-center my-2">
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-blue-600 bg-opacity-50 rounded-md " onClick={() => setEditar(true)}><Pencil size={15} strokeWidth={2.2} /></button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-green-600 bg-opacity-50 rounded-md" onClick={() => setVer(true)}><Eye size={15} strokeWidth={2.2} /> </button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-red-600 bg-opacity-50 rounded-md" onClick={() => setOpen(true)}><Trash size={15} strokeWidth={2.2} /> </button>
+                  </td>
+                </tr>
+                <tr className="border-b dark:border-gray-600">
+                  <td className="text-sm text-gray-900 dark:text-gray-200 py-4">PR1004</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">Envases Universales</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">800-ENVASES</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">Envases</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">contacto@envasesuniversales.com</td>
+                  <td className="flex gap-2 justify-center my-2">
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-blue-600 bg-opacity-50 rounded-md " onClick={() => setEditar(true)}><Pencil size={15} strokeWidth={2.2} /></button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-green-600 bg-opacity-50 rounded-md" onClick={() => setVer(true)}><Eye size={15} strokeWidth={2.2} /> </button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-red-600 bg-opacity-50 rounded-md" onClick={() => setOpen(true)}><Trash size={15} strokeWidth={2.2} /> </button>
+                  </td>
+                </tr>
 
                 <tr className="border-b dark:border-gray-600">
-                  <td className="text-sm text-gray-900 dark:text-gray-200 py-4">10002</td>
-
-                  <td className="text-sm text-gray-900 dark:text-gray-200 whitespace-nowrap">Cebolla morada</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200 whitespace-nowrap">
-                    <span className="px-2 py-1 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 dark:text-green-200 dark:bg-green-800 rounded-full">Fresco</span>
-                  </td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">Q&T SA</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">299</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">16/10/2021</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200 py-4">PR1005</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">Verduras Frescas S.A.</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">800-VERDURAS</td>
                   <td className="text-sm text-gray-900 dark:text-gray-200">Verduras</td>
+                  <td className="text-sm text-gray-900 dark:text-gray-200">contacto@verdurasfrescas.com</td>
                   <td className="flex gap-2 justify-center my-2">
-                    <button className="p-2 text-gray-900 dark:text-gray-200 active:scale-95 transition-transform ease-in-out duration-75 hover:scale-105 transform bg-blue-500 dark:bg-blue-600 rounded-md">
-                      <Pencil size={18} strokeWidth={2} />
-                    </button>
-                    <button className="p-2 text-gray-900 dark:text-gray-200 active:scale-95 transition-transform ease-in-out duration-75 hover:scale-105 transform bg-red-500 dark:bg-red-600 rounded-md" onClick={() => setOpen(true)}>
-                      <Trash size={18} strokeWidth={2} />
-                    </button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-blue-600 bg-opacity-50 rounded-md " onClick={() => setEditar(true)}><Pencil size={15} strokeWidth={2.2} /></button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-green-600 bg-opacity-50 rounded-md" onClick={() => setVer(true)}><Eye size={15} strokeWidth={2.2} /> </button>
+                    <button className="p-1.5 text-gray-900 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-red-600 bg-opacity-50 rounded-md" onClick={() => setOpen(true)}><Trash size={15} strokeWidth={2.2} /> </button>
                   </td>
                 </tr>
-                <tr className="border-b dark:border-gray-600">
-                  <td className="text-sm text-gray-900 dark:text-gray-200 py-4">10003</td>
-
-                  <td className="text-sm text-gray-700 dark:text-gray-200 whitespace-nowrap">Chicharrón de cerdo</td>
-                  <td className="text-sm text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                    <span className="px-2 py-1 text-xs font-medium uppercase tracking-wider text-red-800 bg-red-200 dark:text-red-200 dark:bg-red-800 rounded-full">Caducado</span>
-                  </td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">Carnicería Juan</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">10</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">16/10/2021</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">Carnes</td>
-                  <td className="flex gap-2 justify-center my-2">
-                    <button className="p-2 text-gray-900 dark:text-gray-200 active:scale-95 transition-transform ease-in-out duration-75 hover:scale-105 transform bg-blue-500 dark:bg-blue-600 rounded-md">
-                      <Pencil size={18} strokeWidth={2} />
-                    </button>
-                    <button className="p-2 text-gray-900 dark:text-gray-200 active:scale-95 transition-transform ease-in-out duration-75 hover:scale-105 transform bg-red-500 dark:bg-red-600 rounded-md" onClick={() => setOpen(true)}>
-                      <Trash size={18} strokeWidth={2} />
-                    </button>
-                  </td>
-                </tr>
-                <tr className="border-b dark:border-gray-600">
-                  <td className="text-sm text-gray-900 dark:text-gray-200 py-4">10004</td>
-
-                  <td className=" text-sm text-gray-900 dark:text-gray-200 whitespace-nowrap">Pechuga de pollo</td>
-                  <td className=" text-sm text-gray-900 dark:text-gray-200 whitespace-nowrap">
-                    <span className="px-2 py-1 text-xs font-medium uppercase tracking-wider text-blue-800 bg-blue-200 dark:text-blue-200 dark:bg-blue-800 rounded-full">Vigente</span>
-                  </td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">Pipasa</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">394</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">16/10/2021</td>
-                  <td className="text-sm text-gray-900 dark:text-gray-200">Carnes</td>
-                  <td className="flex gap-2 justify-center my-2">
-                    <button className="p-2 text-gray-900 dark:text-gray-200 active:scale-95 transition-transform ease-in-out duration-75 hover:scale-105 transform bg-blue-500 dark:bg-blue-600 rounded-md">
-                      <Pencil size={18} strokeWidth={2} />
-                    </button>
-                    <button className="p-2 text-gray-900 dark:text-gray-200 active:scale-95 transition-transform ease-in-out duration-75 hover:scale-105 transform bg-red-500 dark:bg-red-600 rounded-md" onClick={() => setOpen(true)}>
-                      <Trash size={18} strokeWidth={2} />
-                    </button>
-                  </td>
-                </tr>
-
               </tbody>
             </table>
           </div>
         </div>
         <Eliminar open={open} onClose={() => setOpen(false)} />
         <Agregar open={agregar} onClose={() => setAgregar(false)} />
+        <Ver open={ver} onClose={() => setVer(false)} />
+        <Editar open={editar} onClose={() => setEditar(false)} />
+
+
       </div>
     </>
   );
--- app/components/pedido/contenido.jsx
+++ app/components/pedido/contenido.jsx
@@ -138,7 +138,7 @@ const TabsDemo = () => {
       </div>
     </div>
           <div className="mb-4 mr-5">
-          <label htmlFor="descripcion" className="block text-sm font-medium text-gray-700 dark:text-gray-200">Descripción</label>
+          <label htmlFor="Observaciones" className="block text-sm font-medium text-gray-700 dark:text-gray-200">Observaciones</label>
           <textarea required id="descripcion" name="descripcion" rows="3" className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"></textarea>
           </div>
           <div className="flex justify-end gap-4 mr-5 ">
