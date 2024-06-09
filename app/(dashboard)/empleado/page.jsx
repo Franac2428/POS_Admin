6b41dcff787c3b6b3e6d4047c1235be5385cb24c
Yaritza Macotelo Menocal <96464552+YariMaco@users.noreply.github.com>
2024-06-08 22:01:47 -0600

    Eliminar

--- app/(dashboard)/empleado/page.jsx
+++ app/(dashboard)/empleado/page.jsx
@@ -10,23 +10,28 @@ import Editar from "@/app/components/empleado/editar";
 import Ver from "@/app/components/empleado/ver";
 import useSWR from 'swr';
 
-
-
-export default function empleado() {
+export default function Empleado() {
     const [open, setOpen] = useState(false);
     const [agregar, setAgregar] = useState(false);
     const [ver, setVer] = useState(false);
     const [editar, setEditar] = useState(false);
     const [evaluar, setEvaluar] = useState(false);
-    const { data, error } = useSWR('http://localhost:3000/api/empleado', async (url) => {
+    const [selectedEmployeeId, setSelectedEmployeeId] = useState(null);
+
+    const { data, error, mutate } = useSWR('http://localhost:3000/api/empleado', async (url) => {
         const response = await fetch(url);
         const data = await response.json();
         return data;
-      });
-    
-      if (error) return <div>Error al cargar los datos</div>;
-      if (!data) return <div>Cargando...</div>;
-      if (!data || !Array.isArray(data)) return <div>No hay datos disponibles</div>; 
+    });
+
+    if (error) return <div>Error al cargar los datos</div>;
+    if (!data) return <div>Cargando...</div>;
+    if (!data || !Array.isArray(data)) return <div>No hay datos disponibles</div>;
+
+    const eliminarEmpleado = (employeeId) => {
+        mutate(data.filter(empleado => empleado.Id !== employeeId), false);
+        setOpen(false);
+    };
 
     return (
         <>
@@ -38,7 +43,6 @@ export default function empleado() {
                     </div>
                     <div className="col-start-8 col-span-3">
                         <div className="flex justify-end gap-6">
-
                             <button className="transition-transform ease-in-out duration-75 hover:scale-105 active:scale-95 transform shadow-lg bg-white dark:bg-gray-700 px-3 py-2 rounded-lg">
                                 <SlidersHorizontal className="text-gray-500 dark:text-gray-400" />
                             </button>
@@ -67,33 +71,35 @@ export default function empleado() {
                                 </tr>
                             </thead>
                             <tbody>
-                            {data.map((empleado) => (
-                                <tr className="" key={empleado.id}>
-                                    <td className=" text-center text-sm text-gray-700 whitespace-nowrap">
-                                        <a href="#" className="font-bold text-blue-700 hover:underline"> {empleado.Id}</a>
-                                    </td>
-                                    <td className="text-center text-sm text-gray-900 dark:text-gray-200 py-4">78930292</td>
-                                    <td className="text-center text-sm text-gray-900 dark:text-gray-200"> {empleado.nombre}</td>
-                                    <td className="text-center text-sm text-gray-900 dark:text-gray-200"> {empleado.apellido}</td>
-                                    <td className=" text-center text-sm text-gray-700 whitespace-nowrap">
-                                        <span className="p-1.5 text-xs font-medium uppercase tracking-wider text-gray-800 bg-red-200 dark:text-red-200 dark:bg-red-800 rounded-lg bg-opacity-50">Cocina</span>
-                                    </td>
-                                    <td className="text-center text-sm text-gray-900 dark:text-gray-200"> {empleado.email}</td>
-                                    <td className="text-center text-sm text-gray-900 dark:text-gray-200"> {empleado.telefono}</td>
-                                    <td className=" flex gap-1 justify-evenly my-1 whitespace-nowrap">
-                                        <button className="p-1.5 text-gray-900 dark:text-gray-200 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-blue-600 bg-opacity-50 rounded-md " onClick={() => setEditar(true)}><Pencil size={15} strokeWidth={2.2} /></button>
-                                        <button className="p-1.5 text-gray-900 dark:text-gray-200 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-green-600 bg-opacity-50 rounded-md" onClick={() => setVer(true)}><Eye size={15} strokeWidth={2.2} /> </button>
-                                        <button className="p-1.5 text-gray-900 dark:text-gray-200 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-yellow-600 bg-opacity-50 rounded-md" onClick={() => setEvaluar(true)}><SmilePlus size={15} strokeWidth={2.2} /></button>
-                                        <button className="p-1.5 text-gray-900 dark:text-gray-200 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-red-600 bg-opacity-50 rounded-md" onClick={() => setOpen(true)}><Trash size={15} strokeWidth={2.2} /> </button>
-                                    </td>
-                                </tr>
-                                        ))}
-
+                                {data.map((empleado) => (
+                                    <tr className="" key={empleado.Id}>
+                                        <td className=" text-center text-sm text-gray-700 whitespace-nowrap">
+                                            <a href="#" className="font-bold text-blue-700 hover:underline"> {empleado.Id}</a>
+                                        </td>
+                                        <td className="text-center text-sm text-gray-900 dark:text-gray-200 py-4">78930292</td>
+                                        <td className="text-center text-sm text-gray-900 dark:text-gray-200"> {empleado.nombre}</td>
+                                        <td className="text-center text-sm text-gray-900 dark:text-gray-200"> {empleado.apellido}</td>
+                                        <td className=" text-center text-sm text-gray-700 whitespace-nowrap">
+                                            <span className="p-1.5 text-xs font-medium uppercase tracking-wider text-gray-800 bg-red-200 dark:text-red-200 dark:bg-red-800 rounded-lg bg-opacity-50">Cocina</span>
+                                        </td>
+                                        <td className="text-center text-sm text-gray-900 dark:text-gray-200"> {empleado.email}</td>
+                                        <td className="text-center text-sm text-gray-900 dark:text-gray-200"> {empleado.telefono}</td>
+                                        <td className=" flex gap-1 justify-evenly my-1 whitespace-nowrap">
+                                            <button className="p-1.5 text-gray-900 dark:text-gray-200 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-blue-600 bg-opacity-50 rounded-md " onClick={() => setEditar(true)}><Pencil size={15} strokeWidth={2.2} /></button>
+                                            <button className="p-1.5 text-gray-900 dark:text-gray-200 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-green-600 bg-opacity-50 rounded-md" onClick={() => setVer(true)}><Eye size={15} strokeWidth={2.2} /> </button>
+                                            <button className="p-1.5 text-gray-900 dark:text-gray-200 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-yellow-600 bg-opacity-50 rounded-md" onClick={() => setEvaluar(true)}><SmilePlus size={15} strokeWidth={2.2} /></button>
+                                            <button className="p-1.5 text-gray-900 dark:text-gray-200 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform bg-red-600 bg-opacity-50 rounded-md" onClick={() => {
+                                                setSelectedEmployeeId(empleado.Id);
+                                                setOpen(true);
+                                            }}><Trash size={15} strokeWidth={2.2} /> </button>
+                                        </td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
                 </div>
-                <Eliminar open={open} onClose={() => setOpen(false)} />
+                <Eliminar open={open} onClose={() => setOpen(false)} employeeId={selectedEmployeeId} onEliminar={eliminarEmpleado} />
                 <Agregar open={agregar} onClose={() => setAgregar(false)} />
                 <Evaluar show={evaluar} onClose={() => setEvaluar(false)} />
                 <Editar open={editar} onClose={() => setEditar(false)} />
--- app/components/empleado/eliminar.jsx
+++ app/components/empleado/eliminar.jsx
@@ -1,37 +1,35 @@
 import { X, CircleAlert } from "lucide-react";
 import { Toaster, toast } from 'sonner';
-import useSWR, { mutate } from 'swr';
 
-export default function Eliminar({ open, onClose, empleado }) {
+export default function Eliminar({ open, onClose, employeeId, onEliminar }) {
     const handleEliminar = async () => {
         try {
-            const response = await fetch(`http://localhost:3000/api/empleado/${empleado.Id}`, {
+            const response = await fetch(`http://localhost:3000/api/empleado/${employeeId}`, {
                 method: 'DELETE',
             });
-
-            if (!response.ok) {
-                throw new Error('Error al eliminar el empleado');
+            const result = await response.json();
+            if (response.ok) {
+                toast.success('Empleado eliminado con éxito');
+                onEliminar(employeeId); 
+            } else {
+                toast.error('Error al eliminar el empleado');
             }
-
-            toast.success('Empleado eliminado con éxito');
-            // Actualiza la lista de empleados después de la eliminación
-            mutate('http://localhost:3000/api/empleado');
-            setTimeout(() => {
-                onClose();
-            }, 1500);
         } catch (error) {
             toast.error('Error al eliminar el empleado');
         }
+        setTimeout(() => {
+            onClose();
+        }, 1000);
     };
 
     return (
-        <div onClick={onClose} className={`fixed inset-0  justify-center items-center grid grid-cols-8 transition-opacity
-        ${open ? "visible bg-black bg-opacity-20" : "invisible"}
-        `}>
-            <div onClick={(e) => e.stopPropagation()} className={` bg-white rounded-xl shadow p-6 transition-all col-span-4 col-start-3
+        <div onClick={onClose} className={`fixed inset-0 justify-center items-center grid grid-cols-8 transition-opacity
+        ${open ? "visible bg-black bg-opacity-20" : "invisible"}`}>
+            <div onClick={(e) => e.stopPropagation()} className={`bg-white rounded-xl shadow p-6 transition-all col-span-4 col-start-3
                 ${open ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}>
                 <button onClick={onClose} className="absolute top-2 right-2 p-1 rounded-lg text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600">
-                    <X /></button>
+                    <X />
+                </button>
                 <div className="w-full">
                     <div className="mx-5 my-4 w-full">
                         <h2 className="text-xl font-bold flex gap-3 text-center text-red-500"><CircleAlert />Confirmar eliminación</h2>
@@ -42,45 +40,17 @@ export default function Eliminar({ open, onClose, empleado }) {
                     </div>
                     <div className="mx-5 my-4 w-full">
                         <div className="flex gap-2">
-                            <p className="text-gray-800 text-md font-bold">Id:</p>
-                            <p className="text-gray-800 text-md">{empleado.Id}</p>
-                        </div>
-                        <div className="flex gap-2">
-                            <p className="text-md text-gray-800 font-bold">Cedula:</p>
-                            <p className="text-gray-800 text-md">Hola</p>
-                        </div>
-                        <div className="flex gap-2">
-                            <p className="text-md text-gray-800 font-bold">Nombre:</p>
-                            <p className="text-gray-800 text-md">{empleado.nombre}</p>
-                        </div>
-                        <div className="flex gap-2">
-                            <p className="text-md text-gray-800 font-bold">Apellidos:</p>
-                            <p className="text-gray-800 text-md">{empleado.apellido}</p>
-                        </div>
-                        <div className="flex gap-2">
-                            <p className="text-md text-gray-800 font-bold">Puesto:</p>
-                            <p className="text-gray-800 text-md">hola</p>
-                        </div>
-                        <div className="flex gap-2">
-                            <p className="text-md text-gray-800 font-bold">Correo:</p>
-                            <p className="text-gray-800 text-md">{empleado.email}</p>
-                        </div>
-                        <div className="flex gap-2">
-                            <p className="text-md text-gray-800 font-bold"># Telefono:</p>
-                            <p className="text-gray-800 text-md">{empleado.telefono}</p>
-                        </div>
-                        <div className="flex gap-2">
-                            <p className="text-md text-gray-800 font-bold">Fecha Contratacion:</p>
-                            <p className="text-gray-800 text-md">hola</p>
-                        </div>
-                        <div className="flex gap-2">
-                            <p className="text-md text-gray-800 font-bold">Fecha Nacimiento:</p>
-                            <p className="text-gray-800 text-md">hola</p>
+                            <p className="text-gray-800 text-md font-bold">ID:</p>
+                            <p className="text-gray-800 text-md">{employeeId}</p>
                         </div>
                     </div>
-                    <div className="flex justify-end gap-4 ">
-                        <button className="bg-red-600 font-semibold rounded-md py-2 px-6 text-white" onClick={handleEliminar}>Eliminar</button>
-                        <button className="bg-gray-400 font-semibold rounded-md py-2 px-6" onClick={onClose}>Cancelar</button>
+                    <div className="flex justify-end gap-4">
+                        <button className="bg-red-600 font-semibold rounded-md py-2 px-6 text-white" onClick={handleEliminar}>
+                            Eliminar
+                        </button>
+                        <button className="bg-gray-400 font-semibold rounded-md py-2 px-6" onClick={onClose}>
+                            Cancelar
+                        </button>
                     </div>
                 </div>
             </div>
