3e1ebccfd28081537dce529788b5b801d361cd5b
vDK-TFK <113957562+vDK-TFK@users.noreply.github.com>
2024-07-10 22:08:15 -0600

    Update inventario
    Add filtros
    Add visualizacion de pedidos
    Add Export de datos excel

--- app/dashboard/pedido/page.jsx
+++ app/dashboard/pedido/page.jsx
@@ -16,7 +16,7 @@ export default function Pedido() {
           <div className="col-start-8 col-span-3 ">
             <div className="flex justify-end gap-6">
             <div className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform shadow-sm bg-verde px-3 py-1 rounded-md">
-              <a  href="/proveedores" className="flex items-center gap-4 shadow-sm active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform
+              <a  href="/dashboard/proveedores" className="flex items-center gap-4 shadow-sm active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform
                   text-white font-semibold px-4 py-1 rounded-md relative flex-1"  >                  
                       <Users className=" text-white-800"/>
                       <div className="">Proveedores</div>
