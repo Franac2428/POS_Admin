dde1039e01b19eed847402f787e0afebe79516fb
Josué Bonilla Soto <josueuniv09@gmail.com>
2024-07-24 18:46:21 -0600

    Reporte listo

--- app/dashboard/reporteria/page.jsx
+++ app/dashboard/reporteria/page.jsx
@@ -11,9 +11,11 @@ export default function Reporteria() {
                     <div>
                         <TopCards />
                     </div>
-                    <div className='mt-6 grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-2 2xl:gap-7.5'>
-                        <div className='w-full'>
-                            <BarChart />
+                    <div className="grid grid-cols-1 md:grid-cols-12 mt-8 mx-auto">
+                        <div className="col-span-12">
+                            <div className='w-full'>
+                                <BarChart />
+                            </div>
                         </div>
                     </div>
                 </div>
